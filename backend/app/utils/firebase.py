bdeebe14aab437696ae50f8e14841059d908e2b4
KennyZakaria <kennyzakaria7@gmail.com>
2026-03-08 16:02:16 +0000

    firebase cred

--- backend/app/utils/firebase.py
+++ backend/app/utils/firebase.py
@@ -1,17 +1,24 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 from app.config import settings
-
+import os
+import json
 _db = None
 
 
-def initialize_firebase():
-    global _db
-    if not firebase_admin._apps:
-        cred = credentials.Certificate(settings.firebase_credentials_path)
-        firebase_admin.initialize_app(cred, {"projectId": settings.project_id})
-    _db = firestore.client()
+# def initialize_firebase():
+#     global _db
+#     if not firebase_admin._apps:
+#         cred = credentials.Certificate(settings.firebase_credentials_path)
+#         firebase_admin.initialize_app(cred, {"projectId": settings.project_id})
+#     _db = firestore.client()
 
+def initialize_firebase():
+    if not firebase_admin._apps:
+        firebase_json = os.environ.get("FIREBASE_CREDENTIALS")
+        cred_dict = json.loads(firebase_json)
+        cred = credentials.Certificate(cred_dict)
+        firebase_admin.initialize_app(cred)
 
 def get_db() -> firestore.Client:
     global _db
